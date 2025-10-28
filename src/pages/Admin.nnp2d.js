c289ccf6f0b7f6a63735b2efecf24448bc59142d
styles137 <ibarwick@me.com>
2025-10-28 17:15:03 -0400

    Update Admin.nnp2d.js

--- src/pages/Admin.nnp2d.js
+++ src/pages/Admin.nnp2d.js
@@ -2,9 +2,10 @@
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
 $w.onReady(function () {
-    // When the image is clicked...
-    $w('#swatch1').onClick(() => {
-        // Change the URL of the iframe
-        $w('#iFrameSwatch').src = "https://www.gvbg.org/swatch1"; // Replace with your target URL
+    //When the swatch is clicked
+    [1, 2, 3, 4].forEach(num => {
+        $w(`#swatch${num}`).onClick(() => {
+            $w('#iFrameSwatch').src = `https://www.gvbg.org/swatch${num}`;
+        });
     });
 });
