c537da5749c4eb8989cb16b1c7ba7b1a51f8f671
styles137 <ibarwick@me.com>
2025-10-28 17:27:36 -0400

    update again

--- src/pages/Admin.nnp2d.js
+++ src/pages/Admin.nnp2d.js
@@ -2,10 +2,34 @@
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
 $w.onReady(function () {
-    //When the swatch is clicked
-    [1, 2, 3, 4].forEach(num => {
-        $w(`#swatch${num}`).onClick(() => {
-            $w('#iFrameSwatch').src = `https://www.gvbg.org/Swatch${num}`;
-        });
+    // When the image is clicked...
+    $w('#swatch1').onClick(() => {
+        // Change the URL of the iframe
+        $w('#iFrameSwatch').src = "https://www.gvbg.org/swatch1"; // Replace with your target URL
     });
+
+    // When the image is clicked...
+    $w('#swatch2').onClick(() => {
+        // Change the URL of the iframe
+        $w('#iFrameSwatch').src = "https://www.gvbg.org/swatch2"; // Replace with your target URL
+    });
+
+    // When the image is clicked...
+    $w('#swatch3').onClick(() => {
+        // Change the URL of the iframe
+        $w('#iFrameSwatch').src = "https://www.gvbg.org/swatch3"; // Replace with your target URL
+    });
+
+    // When the image is clicked...
+    $w('#swatch4').onClick(() => {
+        // Change the URL of the iframe
+        $w('#iFrameSwatch').src = "https://www.gvbg.org/swatch4"; // Replace with your target URL
+    });
+
+    //When the swatch is clicked
+    // [1, 2, 3, 4].forEach(num => {
+    //     $w(`#swatch${num}`).onClick(() => {
+    //         $w('#iFrameSwatch').src = `https://www.gvbg.org/Swatch${num}`;
+    //     });
+    // });
 });
