87b11ec00e61398b6af428c0f2133607e98c4ce8
styles137 <ibarwick@me.com>
2025-10-30 10:24:19 -0400

    d

--- src/pages/Home.q09sd.js
+++ src/pages/Home.q09sd.js
@@ -25,8 +25,9 @@ $w.onReady(async function () {
     console.log("Items:", items);
 
     // Filter out one of them (e.g., hide "Admin")
-    items = items.filter(item => item.label !== "Admin");
     items = items.filter(item => item.label !== "projects");
+    items = items.filter(item => item.label !== "Admin");
+
     console.log("Items:", items);
     // Apply back to the site menu
     $w('#siteMenu1').items = items;
