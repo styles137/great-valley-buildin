6d83fbb25e3294e485e26e3fef699456dca58b5e
styles137 <ibarwick@me.com>
2025-11-04 16:57:37 -0500

    df

--- src/pages/Admin.oxn6n.js
+++ src/pages/Admin.oxn6n.js
@@ -2,9 +2,8 @@
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
 $w.onReady(function () {
-    // Write your JavaScript here
-
-    // To select an element by ID use: $w('#elementID')
-
-    // Click 'Preview' to run your code
+    $w('#AIAssistBtn').onClick(() => {
+        // Change the URL of the iframe
+        $w('#AIiFrame').src = "https://wix-ai-assistant-2f1a896a.base44.app/"; // Replace with your target URL
+    });
 });
